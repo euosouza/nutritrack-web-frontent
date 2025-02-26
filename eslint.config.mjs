6f1f85ca7b1aec281136ade460949ab891d87728
Victor Souza <victor.souza@bulbe.com>
2025-02-26 17:58:05 -0300

    chore: configurando eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "max-len": ["error", { code: 80 }], // Limita a linha a 80 caracteres
+      "quotes": ["error", "double"], // Força o uso de aspas duplas
+      "semi": ["error", "always"], // Exige ponto e vírgula no final das linhas
+    },
+  },
 ];
 
 export default eslintConfig;
